cf65c6f1acb6aa8845dab61607249e6e3f023d56
Abena <kwabea1994@yahoo.co.uk>
2022-01-19 14:21:01 +0000

    adding determinate of matrix

--- Machine_learning/vectors_matrices_and_arrays/calculate_the_average_variance_and_standard_deviation.py
+++ Machine_learning/vectors_matrices_and_arrays/calculate_the_average_variance_and_standard_deviation.py
@@ -3,4 +3,13 @@
 import numpy as np
 
 # create matrix
-matrix = np.array([1, 2, 3],[4, 5, 6], [7, 8, 9])
+matrix = np.array([[1, 2, 3],[4, 5, 6], [7, 8, 9]])
+
+# return mean
+np.mean(matrix)
+
+# return vairance 
+np.var(matrix)
+
+# return standard deviation
+np.std(matrix)
