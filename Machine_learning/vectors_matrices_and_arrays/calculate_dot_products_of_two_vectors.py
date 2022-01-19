677f055092bc7dc97ae3e5e0a962ab6b60c72781
Abena <kwabea1994@yahoo.co.uk>
2022-01-19 14:14:32 +0000

    adding avg, var, and std

---
+++ Machine_learning/vectors_matrices_and_arrays/calculate_dot_products_of_two_vectors.py
@@ -0,0 +1,12 @@
+# load library 
+import numpy as np
+
+# create two vectors 
+vector_a = np.array([1,2,3])
+vector_b = np.array([4,5,6])
+
+# calculate the dot product method one 
+np.dot(vector_a, vector_b)
+
+# calculate the dot product method two  
+vector_a @ vector_b
---
+++ Machine_learning/vectors_matrices_and_arrays/calculate_the_average_variance_and_standard_deviation.py
@@ -0,0 +1,6 @@
+# load libraries 
+
+import numpy as np
+
+# create matrix
+matrix = np.array([1, 2, 3],[4, 5, 6], [7, 8, 9])
