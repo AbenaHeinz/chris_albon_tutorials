8ce82339cb74b4d1c60e6a73e2440c4cb0b6811a
Abena <kwabea1994@yahoo.co.uk>
2022-01-19 17:07:54 +0000

    adding selecting elements

---
+++ Machine_learning/vectors_matrices_and_arrays/selecting_elements_in_an_array.py
@@ -0,0 +1,22 @@
+# load libraries
+import numpy as np
+
+#create row vector 
+vector = np.array([1,2,3,4,5,6])
+
+#select second element 
+vector[1]
+
+# create matrix 
+matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# select element
+
+matrix[1,1]
+
+# create matrix
+tensor = np.array([[[1,1],[1,1]],[[2,2],[2,2]],[[3,3],[3,3]],[[4,4],[4,4]]])
+
+#select second element for each of the three dimenison
+tensor[1,1,1]
+
