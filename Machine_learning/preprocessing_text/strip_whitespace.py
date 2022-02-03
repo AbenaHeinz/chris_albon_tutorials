2e48940e53ff2622e5b3eac74c785f042674511b
Abena <kwabea1994@yahoo.co.uk>
2022-02-03 15:55:24 +0000

    adding strip whitespace

---
+++ Machine_learning/preprocessing_text/strip_whitespace.py
@@ -0,0 +1,9 @@
+# create text  
+
+text_data = ["  Interrobang by Aishawarya Henriette", "Parking and Going. By Karl Gautier","    Today is the night by Jared Prakash"]
+
+# strip whitespace  
+strip_whitespace = [string.strip() for string in text_data]
+
+# show text  
+strip_whitespace
