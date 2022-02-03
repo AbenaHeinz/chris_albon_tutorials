474b8c1d655a6fb928171cd174bdaaa2d72e84b0
Abena <kwabea1994@yahoo.co.uk>
2022-02-03 16:39:51 +0000

    adding tokenize text WIP

---
+++ Machine_learning/preprocessing_text/tokenize_text.py
@@ -0,0 +1,11 @@
+# load libary  
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+# create text  
+string = "The science of today is the technology tomorrow. Tomorrow is today."
+
+#Tokenize words  
+word_tokenize(string)
+
+# tokenized sentence 
+sent_tokenizer(string)
