b3eaac4843c81e7381e5142917c0f7a1292dd8d1
Abena <kwabea1994@yahoo.co.uk>
2022-02-09 17:12:01 +0000

    WIP hyperparameters

---
+++ model_selection/hyperparameter_tuning_using_random_search.py
@@ -0,0 +1,32 @@
+# load libraries  
+from scipy.stats import uniform
+from sklearn import linear_model, datasets
+from sklearn.model_selection import RandomizedSearchCV
+
+
+# load iris  
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# create logistice regression 
+logistic = linear_model.LogisticRegression()
+
+# create regulilization penalty space  
+penalty = ['11', '12']
+
+# Create regualization hyperparameter distribution using uniform distribution  
+C = uniform(loc=0, scale=4)
+
+# create hyer parameter options
+hyperparameters = dict(C=C, penalty = penalty)
+
+# Create Random Search 5-Fold cross validation and 100 iterations 
+clf = RandomizedSearchCV(logistic, hyperparameters, random_state=1, n_iter=100, verbose=0, n_jobs=-1)
+
+# Fit Randomized search
+best_model = clf.fit(X,y)
+
+#veiw best hyperparameter
+print("Best Penalty:", best_model, best_estimator_.get_params()["penalty"])
+print("Best C:", best_model, best_estimator_.get_params()["C"]) 
